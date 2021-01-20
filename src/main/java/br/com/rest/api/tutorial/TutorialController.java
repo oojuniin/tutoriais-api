84be5dc6cd6e5ccde5f5fdb0f65e6a83bab082f3
Junior <juniiorliimatt@gmail.com>
2021-01-19 21:17:45 -0300

    att cors

--- src/main/java/br/com/rest/api/tutorial/TutorialController.java
+++ src/main/java/br/com/rest/api/tutorial/TutorialController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:8081")
+@CrossOrigin(origins = "https://tutoriais-web-oojuniin.herokuapp.com/")
 @RestController
 @RequestMapping("/api")
 public class TutorialController implements Serializable {
